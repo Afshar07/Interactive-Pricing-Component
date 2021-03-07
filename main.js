747f6086dd0c5d5d39d358540831021a1b507ce3
Amir Afshar <mr.afshar0@gmail.com>
2021-03-07 14:54:39 +0330

    Add price list to switch case

--- main.js
+++ main.js
@@ -2,23 +2,30 @@
 const slider = document.getElementById("sRange");
 slider.addEventListener("input", function () {
   let sliderV = slider.value;
+  let price;
   let output;
   switch (sliderV) {
     case "1":
       output = "10k";
+      price = "8.00";
       break;
     case "2":
       output = "50k";
+      price = "12.00";
       break;
     case "3":
       output = "100k";
+      price = "16.00";
       break;
     case "4":
       output = "500k";
+      price = "24.00";
       break;
     case "5":
       output = "1M";
+      price = "36.00";
       break;
   };
   document.getElementById("rSliderValue").innerText = output;
+  document.querySelector(".dollar").innerHTML = price;
 });
