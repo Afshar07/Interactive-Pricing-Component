fd04905bef0affc4e03550da6d6a17838164a331
Amir Afshar <mr.afshar0@gmail.com>
2021-03-09 01:00:44 +0330

    Create the discount algorithm

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-'use strict'
+"use strict";
+// Slider Range
 const slider = document.getElementById("sRange");
+const checkbox = document.getElementById("switch");
 slider.addEventListener("input", function () {
   let sliderV = slider.value;
   let price;
@@ -18,14 +20,31 @@ slider.addEventListener("input", function () {
       price = "16.00";
       break;
     case "4":
-      output = "500k";
+      output = `500 k`;
       price = "24.00";
       break;
     case "5":
       output = "1M";
       price = "36.00";
       break;
+  }
+    if(checkbox.checked){
+    price = ((price / 100) * 75).toFixed(2);
+
   };
   document.getElementById("rSliderValue").innerText = output;
   document.querySelector(".dollar").innerHTML = price;
+
+});
+
+// Switch Toggle
+checkbox.addEventListener("click", function () {
+  let newPrice = document.querySelector(".dollar").innerHTML;
+  if(!this.checked){
+    newPrice = ((newPrice / 3)*4).toFixed(2);
+  }
+  if (this.checked) {
+    newPrice = ((newPrice / 100) * 75).toFixed(2);
+  }
+  document.querySelector(".dollar").innerHTML = newPrice;
 });
