cf1c123e635ec3f835e734b90ed81eeef34b1ba5
Amir Afshar <mr.afshar0@gmail.com>
2021-03-14 00:36:19 +0330

    add a function for slider range background color

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // Slider Range
 const slider = document.getElementById("sRange");
 const checkbox = document.getElementById("switch");
+
 slider.addEventListener("input", function () {
   let sliderV = slider.value;
   let price;
@@ -28,20 +29,26 @@ slider.addEventListener("input", function () {
       price = "36.00";
       break;
   }
-    if(checkbox.checked){
+  if (checkbox.checked) {
     price = ((price / 100) * 75).toFixed(2);
-
-  };
+  }
   document.getElementById("rSliderValue").innerText = output;
   document.querySelector(".dollar").innerHTML = price;
 
+  let value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    "linear-gradient(to right, #a5f3eb 0%, #a5f3eb " +
+    value +
+    "%, #b3b3b3 " +
+    value +
+    "%, #b3b3b3 100%)";
 });
 
 // Switch Toggle
 checkbox.addEventListener("click", function () {
   let newPrice = document.querySelector(".dollar").innerHTML;
-  if(!this.checked){
-    newPrice = ((newPrice / 3)*4).toFixed(2);
+  if (!this.checked) {
+    newPrice = ((newPrice / 3) * 4).toFixed(2);
   }
   if (this.checked) {
     newPrice = ((newPrice / 100) * 75).toFixed(2);
